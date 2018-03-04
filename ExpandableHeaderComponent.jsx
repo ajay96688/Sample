293add69acfd5ef0d72fa529a0bb5e0d0e61acb6
ajay96688 <34381222+ajay96688@users.noreply.github.com>
2018-03-04 14:30:34 +0530

    Add files via upload

---
+++ ExpandableHeaderComponent.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import * as PropTypes from "prop-types";
+
+export default class ExpandableHeaderComponent extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            expanded: null
+        }
+    }
+
+    render() {
+        let arrowClassName = "customExpandButton " + (this.state.expanded ? " expanded" : " collapsed");
+        return <div>
+                    <div className = "customHeaderLabel" > { this.props.displayName } </div>
+                    {
+                        this.props.displayName == 'Name' ? 
+                        <div onClick = { this.expandOrCollapse.bind(this) } className = {arrowClassName}>
+                            <i className = "fa fa-arrow-right"/> 
+                        </div >
+                        : null 
+                    } 
+                </div>
+    }
+
+    expandOrCollapse() {
+        if(!this.state.expanded){
+            this.props.columnApi.setColumnVisible('country', true)
+        }else{
+            this.props.columnApi.setColumnVisible('country', false) 
+        }      
+        this.setState({expanded : !this.state.expanded})
+    }
+}
+
+ExpandableHeaderComponent.propTypes = {
+    params: PropTypes.object
+};
